666fce19a9665d2922cf4d5969da9ea239fd73f6
Marcouss <marc.chamly@gmail.com>
2012-04-04 12:06:43 +0200

    MVC

---
+++ ImageSlider/PuzzlePeace.h
@@ -0,0 +1,13 @@
+//
+//  PuzzlePeace.h
+//  ImageSlider
+//
+//  Created by Marc Chamly on 4/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PuzzlePeace : NSObject
+
+@end
